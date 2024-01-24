0b118a16d77cc1b5c1f15789ed8dbf0e01d81982
Slucajni lik <medicinskicentarr@gmail.com>
2024-01-24 11:12:39 +0100

    aaa

--- millsPytnik/views.py
+++ millsPytnik/views.py
@@ -55,7 +55,7 @@ def playMills(request):
              depth=2
         elif hardness==3:
              dificulty='hard'
-             depth=3
+             depth=2
      
         
         score,_,move=alphaBeta(True,data,currentPlayer,0,depth,float('-inf'),float('inf'),dificulty,currentPlayer)
