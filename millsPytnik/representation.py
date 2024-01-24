c13424bbd516f1e26795263ee95bc14381a609a3
Slucajni lik <medicinskicentarr@gmail.com>
2024-01-24 01:22:47 +0100

    aaa

---
+++ millsPytnik/representation.py
@@ -0,0 +1,30 @@
+#start_statee={"countWhite":0,"countBlack":0,"onBoardWhite":9,"onBoardBlack":3,"pieces":[[[1,1,1],[0,0,0],[1,0,0]],[[0,0,0],[1,0,1],[-1,1,-1]],[[-1,0,0],[0,0,1],[0,1,0]]]}
+#start_statee={"countWhite":0,"countBlack":0,"onBoardWhite":9,"onBoardBlack":3,"pieces":[[[1,1,1],[0,0,0],[1,0,0]],[[0,0,0],[1,0,1],[-1,1,-1]],[[-1,0,0],[0,0,1],[0,1,0]]]}
+#MEDIUM
+
+start_statee={"countWhite":0,"countBlack":0,"onBoardWhite":9,"onBoardBlack":3,"pieces":[[[0,0,1],[0,0,0],[1,-1,-1]],[[0,1,-1],[0,0,0],[1,1,0]],[[0,1,0],[1,0,0],[1,1,0]]]}
+################start_statee={"countWhite":0,"countBlack":0,"onBoardWhite":9,"onBoardBlack":2,"pieces":[[[1,0,-1],[1,0,0],[1,0,1]],[[1,0,-1],[1,0,0],[1,1,0]],[[0,1,0],[0,0,0],[0,0,0]]]}
+
+start_state= {
+        'countWhite': 0,
+        'countBlack': 0,
+        'onBoardWhite':9,
+        'onBoardBlack':8,
+        'pieces': [
+            # square 0
+            [[1,1,1],
+                [-1, 0,-1],
+                [-1,1,-1]],
+            # square 1
+            [[-1,-1,1],
+                [1,0,1],
+                [-1,1,-1]],
+            # square 2
+            [[0,1,0],
+                [0, 0,0],
+                [0,0,0]],
+                ]
+                }
+
+
+
