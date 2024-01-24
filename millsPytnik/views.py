a389c3f5b4a35b9e9bf1bf774b1fccefaed69267
Slucajni lik <medicinskicentarr@gmail.com>
2024-01-24 10:53:20 +0100

    aaa

--- millsPytnik/views.py
+++ millsPytnik/views.py
@@ -55,8 +55,8 @@ def playMills(request):
              depth=2
         elif hardness==3:
              dificulty='hard'
-             depth=3
-    
+             depth=4
+     
         
         score,_,move=alphaBeta(True,data,currentPlayer,0,depth,float('-inf'),float('inf'),dificulty,currentPlayer)
         print("Ovo je potez taj:    ",move)
